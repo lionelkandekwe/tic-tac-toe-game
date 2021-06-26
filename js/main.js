981ea83bda1a0cf76fa24fcd71d2939ed625f2cd
Lionel <liokandekwe@gmail.com>
2021-06-26 19:11:29 +0200

    functionality Tic-tac-toe

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log('Hello')
+import Game from './Game.js'
+import GameView from './GameView.js'
+
+let game = new Game()
+
+let gameView = new GameView()
+
+document.querySelector('.restart').addEventListener('click', () => onRestart())
+
+let tiles = document.querySelectorAll('.board-tile')
+
+tiles.forEach((tile) => {
+  tile.addEventListener('click', () => {
+    onTileClick(tile.dataset.index)
+  })
+})
+
+function onTileClick(i) {
+  // clicked tile
+  game.makeMove(i)
+  gameView.updateBoard(game)
+}
+
+function onRestart() {
+  game = new Game()
+  gameView.updateBoard(game)
+}
+
+gameView.updateBoard(game)
